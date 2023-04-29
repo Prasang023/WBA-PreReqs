1eedb69a63d16a50835e6294c1beaacd5c431527
Prasang023 <prasang023@gmail.com>
2023-04-29 16:34:23 +0530

    correction

---
+++ Rust Tutorial/target/release/deps/hello.d
@@ -0,0 +1,5 @@
+D:\Main\Blockchain\WBA\WBA-PreReqs\Rust Tutorial\target\release\deps\hello.exe: src\main.rs
+
+D:\Main\Blockchain\WBA\WBA-PreReqs\Rust Tutorial\target\release\deps\hello.d: src\main.rs
+
+src\main.rs:
---
+++ Rust Tutorial/target/release/hello.d
@@ -0,0 +1,1 @@
+D:\Main\Blockchain\WBA\WBA-PreReqs\Rust\ Tutorial\target\release\hello.exe: D:\Main\Blockchain\WBA\WBA-PreReqs\Rust\ Tutorial\src\main.rs
